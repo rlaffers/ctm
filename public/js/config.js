99982670fbca3943eb7bf16388aca79ceacb9814
Richard Laffers <rlaffers@gmail.com>
2015-10-19 09:29:15 +0200

    added config.js

---
+++ public/js/config.js
@@ -0,0 +1,9 @@
+//ctm.apiUrl = 'https://ctm.mrdnik.sk/v/1/json/example';
+ctm.apiUrl = 'https://imhd.zoznam.sk/rt/danubehackdata';
+
+// access token for the mapbox account
+ctm.mapboxAccessToken = 'pk.eyJ1IjoicmxhZmZlcnMiLCJhIjoiY2lmdGNyZmZlMDF3N3RkbTBxMmcxcmwzYyJ9.jTqMImbRFKuNS39QXqSuEA';
+ctm.mapboxId = 'rlaffers.nnf9dj3d';
+
+// refresh map for vehicle positions in millisecs
+ctm.refreshRate = 10000;
